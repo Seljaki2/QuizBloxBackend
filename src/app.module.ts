2ea49eaf7032183a5cfb66a55421dd9a70df0f6a
MrDog210 <tusakf@gmail.com>
2025-10-11 15:32:25 +0200

    Added typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST ?? 'localhost',
+      port: Number(process.env.POSTGRES_PORT) ?? 5432,
+      username: process.env.POSTGRES_USERNAME ?? 'quizbox',
+      password: process.env.POSTGRES_PASSWORD ?? 'quizbox',
+      database: process.env.POSTGRES_DATABASE ?? 'quizbox',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
