59ba5b59a670fc0ef73be8ab1a29696b2b91a6f4
Blaž Vidovič <vidovicblaz82@gmail.com>
2025-10-20 18:45:58 +0200

    Create DTOs for creating and updating answers

---
+++ src/answers/dto/create-answer.dto.ts
@@ -0,0 +1,12 @@
+import { IsBoolean, IsString } from 'class-validator';
+
+export class CreateAnswerDto {
+  @IsString()
+  text: string;
+
+  @IsBoolean()
+  isCorrect: boolean;
+
+  @IsString()
+  questionId: string;
+}
