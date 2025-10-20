fa5520ae7f2e1e7b57ed3efa58baa2d9129361b8
Blaž Vidovič <vidovicblaz82@gmail.com>
2025-10-20 18:10:31 +0200

    Implement CRUD operations for questions

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { QuestionsService } from './questions.service';
+import { Question } from './entities/question.entity';
+import { CreateQuestionDto } from './dto/create-question.dto';
+import { UpdateQuestionDto } from './dto/update-question.dto';
+import { DeleteResult } from 'typeorm';
 
 @Controller('questions')
-export class QuestionsController {}
+export class QuestionsController {
+  constructor(private readonly questionsService: QuestionsService) {}
+
+  @Get()
+  async findAll(): Promise<Question[]> {
+    return this.questionsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Question | null> {
+    return this.questionsService.findOne(id);
+  }
+
+  @Post()
+  async create(@Body() createQuestionDto: CreateQuestionDto) {
+    return await this.questionsService.create(createQuestionDto);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateQuestionDto: UpdateQuestionDto,
+  ) {
+    return await this.questionsService.update(id, updateQuestionDto);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<DeleteResult> {
+    return await this.questionsService.delete(id);
+  }
+}
--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Question } from './entities/question.entity';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { CreateQuestionDto } from './dto/create-question.dto';
+import { UpdateQuestionDto } from './dto/update-question.dto';
 
 @Injectable()
-export class QuestionsService {}
+export class QuestionsService {
+  constructor(
+    @InjectRepository(Question)
+    private readonly questionsRepository: Repository<Question>,
+  ) {}
+
+  async findAll(): Promise<Question[]> {
+    return await this.questionsRepository.find();
+  }
+
+  async findOne(id: string): Promise<Question | null> {
+    return await this.questionsRepository.findOneBy({ id: id });
+  }
+
+  async create(createQuestionDto: CreateQuestionDto): Promise<Question> {
+    return await this.questionsRepository.save(
+      this.questionsRepository.create(createQuestionDto),
+    );
+  }
+
+  async update(
+    id: string,
+    updateQuestionDto: UpdateQuestionDto,
+  ): Promise<UpdateResult> {
+    return this.questionsRepository.update({ id: id }, updateQuestionDto);
+  }
+
+  async delete(id: string): Promise<DeleteResult> {
+    return await this.questionsRepository.delete({ id: id });
+  }
+}
