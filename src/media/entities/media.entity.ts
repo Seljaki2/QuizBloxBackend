c28e7bdaa776a36541584669079f29546f71bc66
MrDog210 <tusakf@gmail.com>
2025-10-25 00:32:40 +0200

    Update media.entity.ts

--- src/media/entities/media.entity.ts
+++ src/media/entities/media.entity.ts
@@ -1,8 +1,21 @@
+import { unlink } from 'fs';
+import path from 'path';
 import { Base } from 'src/common/entities/base.entity';
-import { Column, Entity } from 'typeorm';
+import { AfterRemove, BeforeRemove, Column, Entity } from 'typeorm';
+import { UPLOAD_DESTINATION } from '../upload-destination';
 
 @Entity()
 export class Media extends Base {
   @Column({ nullable: false })
   path: string;
+
+  @BeforeRemove()
+  removeFile() {
+    console.log('Deleting file at path:');
+    unlink(path.join(UPLOAD_DESTINATION, this.path), (err) => {
+      if (err) {
+        console.error('Error deleting file:', err);
+      }
+    });
+  }
 }
