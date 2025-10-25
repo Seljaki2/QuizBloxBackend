13d8421b317dce89f9091d085e8888e8111ddf3b
MrDog210 <tusakf@gmail.com>
2025-10-25 22:25:08 +0200

    Fix file path

--- src/media/upload-destination.ts
+++ src/media/upload-destination.ts
@@ -1,3 +1,5 @@
 import { join } from 'path';
 
-export const UPLOAD_DESTINATION = join(process.cwd(), 'uploads');
+export const UPLOAD_DESTINATION =
+  process.env.MEDIA_DIR ?? join(process.cwd(), 'uploads');
+console.log('FILE UPLOAD DIR', UPLOAD_DESTINATION);
