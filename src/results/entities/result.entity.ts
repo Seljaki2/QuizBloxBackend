b5b708f480fbfb19c11ab5ffca0d72ef581ba207
Blaž Vidovič <vidovicblaz82@gmail.com>
2025-10-28 22:10:22 +0100

    Add quiz and question relationships to result entity
    - Make answer property nullable in result entity

--- src/results/entities/result.entity.ts
+++ src/results/entities/result.entity.ts
@@ -2,6 +2,8 @@ import { Column, Entity, ManyToOne } from 'typeorm';
 import { Base } from '../../common/entities/base.entity';
 import { Answer } from '../../answers/entities/answer.entity';
 import { User } from '../../users/entities/user.entity';
+import { Quiz } from '../../quizzes/entities/quiz.entity';
+import { Question } from '../../questions/entities/question.entity';
 
 @Entity()
 export class Result extends Base {
@@ -11,7 +13,13 @@ export class Result extends Base {
   @Column({ nullable: true })
   username?: string;
 
-  @ManyToOne(() => Answer, (answer) => answer.results, { nullable: false })
+  @ManyToOne(() => Quiz)
+  quiz: Quiz;
+
+  @ManyToOne(() => Question, { nullable: true })
+  question?: Question;
+
+  @ManyToOne(() => Answer, (answer) => answer.results, { nullable: true })
   answer: Answer;
 
   @Column({ nullable: true })
